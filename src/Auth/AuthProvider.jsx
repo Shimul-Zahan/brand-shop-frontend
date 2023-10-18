71dea01a77b4d93941668b0249638bb801f58cbe
AlgoWebBot <shimulzahan636@gmail.com>
2023-10-18 11:19:40 +0600

    create context and private route added

--- src/Auth/AuthProvider.jsx
+++ src/Auth/AuthProvider.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react'
+import auth from '../Firebase/firebase.config';
 
-const AuthProvider = () => {
-  return (
-    <div>AuthProvider</div>
-  )
+export const MyContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvide = new GoogleAuthProvider();
+
+    const passwordRegistration = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvide)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        })
+        return (() => {
+            unSubscribe();
+        })
+    }, [])
+
+    const contenxtProperty = {
+        user,
+        loading,
+        passwordRegistration,
+        logIn,
+        googleLogin,
+        logOut,
+    }
+
+    return (
+        <MyContext.Provider value={contenxtProperty}>
+            {children}
+        </MyContext.Provider>
+    )
 }
 
 export default AuthProvider
